cded3a99491d22d1e6c969025fd7d7c7904aa542
tarekkhalifa1 <user.tarek@outlook.com>
2022-10-08 12:52:51 +0200

    modified in css

--- index.php
+++ index.php
@@ -112,14 +112,11 @@ if (isset($_POST['submit'])) {
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <style>
-        .registeration {
-            display: block;
             <?php
             if ($show_data === true) {
-                echo "display: none;";
+                echo ".registeration {display: none;}";
             }
             ?>
-        }
 
         table {
             padding: 15px;
